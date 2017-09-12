5106966b2899dc9221f1175e987ac38b9be9da3a
John Pope <jp@bellgeorge.com>
2017-09-12 11:37:08 -0400

    fixes to file name
    - due to restrictions on docker-compose - see https://github.com/tchaudhry91/rainbow-sha256-go/pull/4/commits/6179884d97b6e151b2029b3467c2aeed34809969

--- scratch_builder.sh
+++ scratch_builder.sh
@@ -2,10 +2,10 @@
 
 TEMP_CONTAINER_NAME=hasher-builder-`git rev-parse --short --verify HEAD`
 BRANCH=$1
-docker build -t tchaudhry/rainbow-hasher-go:scratch-builder -f Dockerfile.full .
+docker build -t tchaudhry/rainbow-hasher-go:scratch-builder -f ./full/Dockerfile .
 docker run --name=$TEMP_CONTAINER_NAME tchaudhry/rainbow-hasher-go:scratch-builder 
 docker cp $TEMP_CONTAINER_NAME:/app/bin ./
 docker container rm $TEMP_CONTAINER_NAME
-docker build -t tchaudhry/rainbow-hasher-go:$BRANCH -f Dockerfile.scratch .
+docker build -t tchaudhry/rainbow-hasher-go:$BRANCH -f ./scratch/Dockerfile .
 rm -rf bin
 docker push tchaudhry/rainbow-hasher-go:$BRANCH
